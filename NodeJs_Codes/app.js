5b9d2700006c52aa0554d2b986f1dc4e8b04f45b
Abdulwahab1127 <Abdulwahab1_7@hotmail.com>
2025-09-19 23:22:25 +0500

    Used Express App.use etc , Middlewares, Routes Basics Etc Today

--- NodeJs_Codes/app.js
+++ NodeJs_Codes/app.js
@@ -1,11 +1,30 @@
-const http = require('http');
 
 const express = require('express');
-
-
-
 const app = express();
 
-const server = http.createServer((req,res)=>{
+// app.use((req,res,next) =>{
 
-});
+//     console.log("Using Middlewear!");
+//     next();//this allow the request to continue to next middlewear
+
+// })
+
+app.use('/user',(req,res,next) =>{
+
+    console.log("Using User Middlewear!");
+    res.send('<h1>Hello From USER YAYYY!</h1>');
+
+})
+
+
+app.use((req,res,next) =>{
+
+    console.log("Using Default Middlewear!");
+    res.send('<h1>Hello From Express!</h1>');
+
+})
+
+// const server = http.createServer(app);
+// server.listen(3000);
+//We can Remove this in Express and Only use 
+app.listen(3000);
